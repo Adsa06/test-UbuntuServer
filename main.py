087db18ca6e454e1e8a8f12bf5c8bfb81f25ec01
Aitor De Santos Amorós <74840018+Adsa06@users.noreply.github.com>
2025-12-26 19:24:33 +0100

    Update database connection and add /users endpoint
    Refactor database connection and add user retrieval endpoint.

--- main.py
+++ main.py
@@ -1,51 +1,45 @@
 from fastapi import FastAPI
 import mysql.connector
 from pydantic import BaseModel
+from typing import List
 
-# Inicializamos FastAPI
 app = FastAPI()
 
-# Conexión a la base de datos MySQL
+# Configuración de la base de datos
+MYSQL_HOST = "mysql"
+MYSQL_PORT = 3306
+MYSQL_USER = "adsa"
+MYSQL_PASSWORD = "2006"
+MYSQL_DB = "test_db"
+
+# Definir el modelo de los usuarios
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: str
+
+# Función para conectar a la base de datos
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="mysql",  # Nombre del servicio de MySQL en docker-compose.yml
-        user="user",   # Usuario de la base de datos
-        password="userpassword",  # Contraseña de la base de datos
-        database="test_db"  # Nombre de la base de datos
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DB
     )
     return connection
 
-# Definir un modelo de datos para la API
-class Item(BaseModel):
-    name: str
-    description: str
+@app.get("/users", response_model=List[User])
+def get_users():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return users
 
 # Ruta para ver un mensaje
 @app.get("/")
 def read_root():
     return {"message": "¡Hola Mundo! Este es un ejemplo de FastAPI con Docker."}
-
-# Ruta para obtener datos desde la base de datos MySQL
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM items WHERE id = %s", (item_id,))
-    item = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    
-    if item is None:
-        return {"message": "Item no encontrado"}
-    return item
-
-# Ruta para agregar un nuevo item a la base de datos
-@app.post("/items/")
-def create_item(item: Item):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO items (name, description) VALUES (%s, %s)", (item.name, item.description))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return {"message": "Item creado correctamente"}
